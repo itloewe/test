9478491436425007beae61a5442faae02b3d460d
owais <owais@192.168.0.151>
2019-05-05 18:44:35 +0200

    first commit

---
+++ src/test/java/lecture/TestJavaLecture.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.java.lecture;
+
+/**
+ *
+ * @author owais
+ */
+public class TestJavaLecture {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // let's figure the area of a circle
+        double radius = 15;
+        double area;
+        area = Math.PI * radius * radius;
+        System.out.println(area);
+        // TODO code application logic here
+        
+    }
+    
+}
